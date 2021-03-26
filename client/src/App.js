d7fc8a9f94695256130c03630c5bfc4bb0bc203f
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-26 11:05:01 +0100

    front: add reactJS and add some confin into package.json

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppNavBar from "./components/AppNavBar";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <AppNavBar />
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
